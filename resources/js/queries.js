11ca9c6598eae0fc794ce53c5d0da231c80bb21b
Karl <karllouiserito08@gmail.com>
2022-03-29 22:34:26 +0800

    Display Image - Test - Success

--- resources/js/queries.js
+++ resources/js/queries.js
@@ -50,7 +50,7 @@ let queries = {
         }
     }`,
 
-    customer: `{customer{firstname,lastname, email, image}}`,
+    customer: `{customer{firstname,lastname, email, image, id}}`,
 
     updateProfile: `mutation updateProfile($file: Upload!, $customer: customerInput) {
         updateProfile(file: $file, customer: $customer)
